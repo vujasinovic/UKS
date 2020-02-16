5e25688295cdd6f73a332f7f80f6e36bcfd951f5
Aleksandar Vujasinovic <aleksandar.vujasinov@gmail.com>
2020-02-16 09:28:49 +0100

    Change User column name to username

--- uxhub/models.py
+++ uxhub/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
     email = models.EmailField()
 
 
